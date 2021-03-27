6d271219daceb9323414e2d39517d55ece0df472
Minal Bansal <minalbansal14@gmail.com>
2021-03-27 15:34:11 +0530

    food random

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import pygame
+import random
 pygame.init()
-
-
-#creating Window
-gamewindow = pygame.display.set_mode((800,500))
-pygame.display.set_caption("My First Project")
 
 #Colors
 white = (255, 255, 255)
@@ -13,6 +9,17 @@ black = (0, 0, 0)
 
 #Game specific Variables
 exit_game = False
+window_width=800
+window_height=500
+snake_x=50
+snake_y=50
+snake_size=5
+food_x=random.randint(5,window_width)
+food_y=random.randint(5,window_height)
+
+#creating Window
+gamewindow = pygame.display.set_mode((window_width,window_height))
+pygame.display.set_caption("My First Project")
 
 
 #Creating a game loop
@@ -26,7 +33,8 @@ while not exit_game:
 
 
     gamewindow.fill(white)
-    pygame.draw.rect(gamewindow,black,(50,50,5,5))
+    pygame.draw.rect(gamewindow,black,(snake_x,snake_y,snake_size,snake_size))
+    pygame.draw.rect(gamewindow,red,(food_x,food_y,snake_size,snake_size))
     pygame.display.update()
 pygame.quit()
 quit()
